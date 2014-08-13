6c093345c5522b6a7e4aebba7422c141b4369f0f
yijingbai <everatnight@gmail.com>
2014-08-13 20:40:54 +0800

    delete the basicInfo

--- lib/models/model.py
+++ lib/models/model.py
@@ -78,17 +78,3 @@ class Price(db.Model):
         self.parking_id = parking_id
         self.hour = hour
         self.day = day
-
-
-class BasicInfo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    uid = db.Column(db.String(40))
-    total_pak = db.Column(db.Integer)
-    info = db.Column(db.Text)
-
-    def __init__(self, json_dic):
-        self.name = json_dic['name']
-        self.uid = json_dic['uid']
-        self.total_pak = json_dic['total_pak']
-        self.info = ';'.join(json_dic['info'])
